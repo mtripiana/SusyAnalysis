6142e217579ccb5325cb60d354b85648df36289e
tripiana <tripiana@4525493e-7705-40b1-a816-d608a930855b>
2015-03-03 10:35:34 +0000

    Update pkgs.

--- scripts/get_extra_packages.sh
+++ scripts/get_extra_packages.sh
@@ -11,6 +11,7 @@ cd $ANALYSISCODE
 rc checkout_pkg $SVNOFF/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-05-00-20 # (needs patching. see below)
 
 
+rc checkout_pkg $SVNOFF/PhysicsAnalysis/ElectronPhotonID/ElectronEfficiencyCorrection/tags/ElectronEfficiencyCorrection-00-01-19
 rc checkout_pkg $SVNOFF/Reconstruction/EventShapes/EventShapeTools/tags/EventShapeTools-00-01-07  #(needs patching)
 rc checkout_pkg $SVNOFF/Reconstruction/Jet/JetAnalysisTools/JVFUncertaintyTool/tags/JVFUncertaintyTool-00-00-04 
 
